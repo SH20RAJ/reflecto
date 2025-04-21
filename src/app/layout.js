c1c9c4967126811fbe3d297cbecbce4d16305d73
SH20RAJ <shaswatraj3@gmail.com>
2025-04-22 02:16:52 +0530

    feat: add dark mode class to HTML element for improved styling

--- src/app/layout.js
+++ src/app/layout.js
@@ -23,7 +23,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
-      <html lang="en">
+      <html lang="en" className="dark">
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
