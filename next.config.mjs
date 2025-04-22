f8775b766e767ce10cb2fa803d96497dbd0922e6
SH20RAJ <shaswatraj3@gmail.com>
2025-04-23 04:32:56 +0530

    chore: update dependencies and configuration files
    - Added postinstall script to package.json for husky installation.
    - Updated tailwindcss and added tailwindcss-animate to devDependencies.
    - Configured postcss with tailwindcss and autoprefixer.
    - Added .node-version and .nvmrc files to specify Node.js version 20.17.0.
    - Created .npmrc file to enforce legacy peer dependencies and engine strictness.
    - Included tailwindcss-animate plugin in tailwind.config.js.

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'standalone',
+  images: {
+    domains: ['lh3.googleusercontent.com', 'avatars.githubusercontent.com'],
+    formats: ['image/avif', 'image/webp'],
+  },
+  experimental: {
+    serverComponentsExternalPackages: ['@libsql/client'],
+  },
+};
 
 export default nextConfig;
